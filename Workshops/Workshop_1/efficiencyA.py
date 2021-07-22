3b4cd44dc3774600d19e1b8359d6ab24f529416c
Honeypot <juansehgu@gmail.com>
2021-07-21 21:04:20 -0500

    Updated

--- Workshops/Workshop_1/efficiencyA.py
+++ Workshops/Workshop_1/efficiencyA.py
@@ -2,13 +2,13 @@ import numpy as np
 import sympy as sym
 from sympy.core.sympify import sympify
 from sympy.polys.polytools import factor
-x, y = sym.symbols('x y')
+x = sym.symbols('x')
 expre = "x**3-2*x**2+4*x/3-8/27"
 cont = 0
 try:
     expre = factor(expre)
     cont += 1
-    print(sym.sympify(expre).subs(x, 4))
-    print(cont)
+    #print(sym.sympify(expre).subs(x, 4))
+    print(expre)
 except:
     print("sad")
